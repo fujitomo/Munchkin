bf2b95fe8769cdd3ec93fd86e2108ef612abf7aa
tomohiro fujino <fujitomo375@gmail.com>
2019-12-14 12:48:41 +0900

    a

--- MOSHA/js/event.js
+++ MOSHA/js/event.js
@@ -1,5 +1,6 @@
 $ (function () {
-  $ ('#btn1, #btn2').on ('click', function () {
+  //資料請求フォームへ移動
+  $ ('#btn1, #btn2,#btn3').on ('click', function () {
     // スクロールの速度
     var speed = 200; // ミリ秒
     // アンカーの値取得
@@ -15,10 +16,55 @@ $ (function () {
     $ ('html, body').animate ({scrollTop: 0}, speed, 'swing');
     return true;
   });
-  $ ('#acd_label').click (function (e) {
-    e.style.textDecoration = 'underline';
+
+  $ ('*').on ('click', function () {
+    $ ('.q').removeClass ('click_mark');
+    $ ('.box_title').removeClass ('click_mark');
   });
 
+  //アコーディオン1
+
+  $ ('.qa-box').on ('click', function () {
+    var target = $ (event.target);
+    var answer = $ (this).find ('div');
+    if ($ (answer[1]).hasClass ('hide')) {
+      $ (answer[1]).stop (true, true).slideToggle ();
+      $ (answer[1]).removeClass ('hide');
+      $ (answer[0]).addClass ('open');
+      $ (answer[0]).removeClass ('close');
+    } else if (!$ (this).next ().hasClass ('hide')) {
+      $ (answer[1]).stop (true, true).slideToggle ();
+      $ (answer[1]).addClass ('hide');
+      $ (answer[0]).removeClass ('open');
+      $ (answer[0]).addClass ('close');
+    }
+    $ (answer[0]).addClass ('click_mark');
+  });
+
+  //アコーディオン2
+
+  $ ('.box_container').on ('click', function () {
+    var w = $ (window).width ();
+    var x = 769;
+    if (w <= x) {
+      var target = $ (event.target);
+      var answer = $ (this).find ('div');
+      if ($ (answer[1]).hasClass ('hide')) {
+        $ (answer[1]).stop (true, true).slideToggle ();
+        $ (answer[1]).removeClass ('hide');
+        $ (answer[0]).addClass ('open');
+        $ (answer[0]).removeClass ('close');
+      } else if (!$ (this).next ().hasClass ('hide')) {
+        $ (answer[1]).stop (true, true).slideToggle ();
+        $ (answer[1]).addClass ('hide');
+        $ (answer[0]).removeClass ('open');
+        $ (answer[0]).addClass ('close');
+      }
+      $ (answer[0]).addClass ('click_mark');
+    }
+  });
+
+  //topに戻る
   $ (window).on ('load scroll', function () {
     var vScrollDistance = $ (window).scrollTop ();
     var element = $ ('#back_to_top');
